77d9eefd8a94c3de985ec4cafd323c863e9c6f47
moyushushi <16016346+moyushushi@user.noreply.gitee.com>
2025-10-19 12:57:29 +0800

    start

---
+++ study-project-backed/src/main/java/com/test/studyprojectbacked/StudyProjectBackedApplication.java
@@ -0,0 +1,13 @@
+package com.test.studyprojectbacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectBackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjectBackedApplication.class, args);
+    }
+
+}
---
+++ study-project-backed/src/test/java/com/test/studyprojectbacked/StudyProjectBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.studyprojectbacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectBackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
